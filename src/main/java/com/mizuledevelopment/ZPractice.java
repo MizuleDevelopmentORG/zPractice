f92cdbc634330eb22bb2fc6af64ebb34f4e449f3
Kacper Mikolajczyk <vkacperpl@gmail.com>
2023-08-23 15:55:35 +0200

    Project setup - gradle

---
+++ src/main/java/com/mizuledevelopment/ZPractice.java
@@ -0,0 +1,18 @@
+package com.mizuledevelopment;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZPractice extends JavaPlugin {
+
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
