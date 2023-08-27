182f676097f72b068e2f289b473a282164bba1e6
powercas_gamer <cas@deltapvp.net>
2023-08-27 04:02:24 +0200

    spotless

--- src/main/java/com/mizuledevelopment/zpractice/util/helper/SignHelper.java
+++ src/main/java/com/mizuledevelopment/zpractice/util/helper/SignHelper.java
@@ -1,6 +1,5 @@
 package com.mizuledevelopment.zpractice.util.helper;
 
-import com.google.gson.reflect.TypeToken;
 import com.mizuledevelopment.zpractice.sign.DataSign;
 import com.mizuledevelopment.zpractice.util.LazyLocation;
 import com.mizuledevelopment.zpractice.zPractice;
@@ -8,8 +7,8 @@ import org.bson.Document;
 
 public class SignHelper {
 
-    public static Document from(DataSign dataSign){
-        Document document = new Document();
+    public static Document from(final DataSign dataSign) {
+        final Document document = new Document();
         document.put("name", dataSign.getName());
         document.put("arena", dataSign.getArena());
         document.put("kit", dataSign.getKit());
@@ -18,7 +17,7 @@ public class SignHelper {
         return document;
     }
 
-    public static DataSign from(Document document) {
+    public static DataSign from(final Document document) {
         return new DataSign(document.getString("name"), document.getString("arena"), document.getString("kit"),
             zPractice.GSON.fromJson(document.getString("location"), LazyLocation.class), document.getInteger("players"));
     }
