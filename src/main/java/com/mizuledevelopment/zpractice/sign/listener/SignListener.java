344f19f5fef6dbd5db7d54f3875266928b11d34d
KacpersDev <vkacperpl@gmail.com>
2023-08-27 18:55:51 +0200

    messages

--- src/main/java/com/mizuledevelopment/zpractice/sign/listener/SignListener.java
+++ src/main/java/com/mizuledevelopment/zpractice/sign/listener/SignListener.java
@@ -2,6 +2,9 @@ package com.mizuledevelopment.zpractice.sign.listener;
 
 import com.destroystokyo.paper.MaterialSetTag;
 import com.mizuledevelopment.zpractice.util.LazyLocation;
+import com.mizuledevelopment.zpractice.util.color.MessageType;
+import com.mizuledevelopment.zpractice.util.color.TextUtil;
+import com.mizuledevelopment.zpractice.util.config.Config;
 import com.mizuledevelopment.zpractice.zPractice;
 import com.mizuledevelopment.zpractice.queue.Queue;
 import com.mizuledevelopment.zpractice.sign.DataSign;
@@ -11,6 +14,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import javax.swing.*;
 import java.util.*;
 
 public class SignListener implements Listener {
@@ -31,16 +35,27 @@ public class SignListener implements Listener {
 
             if (dataSign != null) {
                 if (this.plugin.getQueueManager().containsPlayer(event.getPlayer().getUniqueId())) {
-                    Bukkit.broadcastMessage("you already in queue");
+                    event.getPlayer().sendMessage(TextUtil.parse(this.plugin.getMessages()
+                        .getConfiguration().getString("queue-in-queue"), MessageType.from(Objects.requireNonNull(this.plugin.getConfiguration()
+                        .getConfiguration().getString("queue-in-queue")))));
                     return;
                 }
-
                 if (!this.plugin.getQueueManager().has(dataSign.getName())) {
                     this.plugin.getQueueManager().getQueues().add(new Queue(dataSign.getName(), dataSign.getArena(), dataSign.getKit(), new HashSet<>(Collections.singletonList(event.getPlayer().getUniqueId())), dataSign.getMaxPlayers()));
-                    Bukkit.broadcastMessage("added you to queue");
+                    event.getPlayer().sendMessage(TextUtil.parse(Objects.requireNonNull(this.plugin.getMessages()
+                            .getConfiguration().getString("queue-joined"))
+                        .replace("%arena%", dataSign.getArena())
+                        .replace("%kit%", dataSign.getKit())
+                        .replace("%name%", dataSign.getName())
+                        .replace("%players%", String.valueOf(this.plugin.getQueueManager().get(dataSign.getName()).getPlayers().size()))
+                        .replace("%max%", String.valueOf(dataSign.getMaxPlayers())), MessageType.from(Objects.requireNonNull(this.plugin.getMessages()
+                        .getConfiguration().getString("queue-joined")))));
                 } else {
                     this.plugin.getQueueManager().get(dataSign.getName()).getPlayers().add(event.getPlayer().getUniqueId());
-                    Bukkit.broadcastMessage("added you to queue with second method");
+                    event.getPlayer().sendMessage(TextUtil.parse(this.plugin.getMessages()
+                        .getConfiguration().getString("queue-joined"), MessageType.from(Objects.requireNonNull(this.plugin.getMessages()
+                        .getConfiguration().getString("queue-joined")))));
+
                     if (this.plugin.getQueueManager().get(dataSign.getName()).getPlayers().size() == dataSign.getMaxPlayers()) {
                         // todo move players
                     }
@@ -49,3 +64,4 @@ public class SignListener implements Listener {
         }
     }
 }
+
